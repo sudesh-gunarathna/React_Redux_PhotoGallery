75410c2b401e0fb0bee3aa6c5bec396d501d5aa6
sudesh <sudesh.jeewan@gmail.com>
2023-09-30 07:02:51 +0530

    data fetching & data mapping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { useSelector } from 'react-redux';
 import './App.css';
 import DarkModeToggle from './components/DarkModeToggle';
+import ImageGrid from './components/ImageGrid';
 
 function App() {
   const darkMode = useSelector((state)=>state.theme.darkMode)
@@ -10,6 +11,7 @@ function App() {
     <div className={`py-5 min-h-screen  ${darkMode?"bg-black":"bg-white"}`}>
       <h1 className={`text-4xl font-bold text-center font-mono ${darkMode? "text-white": "text-black"}` }>Photo Gallery</h1>
       <DarkModeToggle/>
+      <ImageGrid/>
     </div>
   );
 }
